bd4df25fd0397a2c3b1093429c5b2dcf8735f5d2
sibofeng <fengsiboacer@gmail.com>
2017-10-28 16:49:20 +0800

    increament version

--- src/IPILAE.py
+++ src/IPILAE.py
@@ -1,4 +1,7 @@
-
+import sys
+import os
+workpath = os.path.abspath("..")
+sys.path.append(workpath)
 import numpy as np
 from sklearn import preprocessing
 import random
@@ -9,37 +12,34 @@ import pickle
 import csv
 import h5py
 import pandas as pd
+import src.tools as tools
 
 
 class PILAE(object):
 
-    def __init__(self,HiddernNeurons,batchsize,k,para,actFun):
-        self.HiddernNeurons = HiddernNeurons          # 隐层神经元
-        self.batchsize = batchsize            # 批次大小
-        self.k = k             # 误差
-        self.para = para            # sigmoid p值
-        self.actFun = actFun            # 激活函数
-        self.layers = len(HiddernNeurons)   #隐层层数
-        self.w=list(range(self.layers))        #权重
-        self.preiS=list(range(self.layers))        #is
-        self.InputWeight=list(range(self.layers))       #输入权重
+    def __init__(self, HiddernNeurons, batchsize, k, actFun='sig'):
+        self.HiddernNeurons = HiddernNeurons
+        self.batchsize = batchsize
+        self.k = k
+        self.actFun = actFun
+        self.layers = len(HiddernNeurons)
+        self.w = list(range(self.layers))
+        self.preiS = list(range(self.layers))
+        self.InputWeight = list(range(self.layers))
 
     def makebatches(self, data):
-
         randomorder = random.sample(range(len(data)), len(data))
         numbatches = int(len(data) / self.batchsize)
         for batch in range(numbatches):
             order = randomorder[batch * self.batchsize:(batch + 1) * self.batchsize]
             yield data[order, :]
 
-    def train(self, data):
-
+    def fit(self, data):
         hidden_weight = []
         hidden_layer = []
-        error_i = []
-        y_out = []
-        for numbatch,delta_X in enumerate(self.makebatches(data)):
+        for numbatch, delta_X in enumerate(self.makebatches(data)):
             for i in range(self.layers):
+                print("the "+str(i)+" th layer"+"the " + str(numbatch) + " th batch")
                 U, s, V = np.linalg.svd(delta_X)
                 V = V.T
                 V = V[:, :self.HiddernNeurons[i]]
@@ -54,12 +54,12 @@ class PILAE(object):
 
                 if numbatch ==0:
                     H = delta_X.dot(self.InputWeight[i])
-                    delta_H = ActivationFunc(H, self.actFun[i], self.para[i])
+                    delta_H = self.activeFunction(H, self.actFun)
                     iS = np.linalg.inv(delta_H.T.dot(delta_H) + np.eye(delta_H.shape[1]) * self.k[i])
                     OW = iS.dot(delta_H.T).dot(delta_X)
                 else:
                     H = delta_X.dot(self.w[i].T)
-                    delta_H = ActivationFunc(H, self.actFun[i], self.para[i])
+                    delta_H = self.activeFunction(H, self.actFun)
                     iC = np.linalg.inv(np.eye(delta_H.shape[0]) + delta_H.dot(self.preiS[i]).dot(delta_H.T))
                     iS = self.preiS[i] - self.preiS[i].dot(delta_H.T).dot(iC).dot(delta_H).dot(self.preiS[i])
                     alpha = np.eye(delta_H.shape[1]) - self.preiS[i].dot(delta_H.T).dot(iC).dot(delta_H)
@@ -68,91 +68,85 @@ class PILAE(object):
                         delta_H.T).dot(delta_X)
                     OW = alpha.dot(self.w[i]) + belta
                 hidden_layer.append(delta_H)
-                delta_X0 = delta_H.dot(self.InputWeight[i].T)
-                error = np.linalg.norm(delta_X0 - delta_X)
-                print(error)
-                y_out.append(delta_X0)
-                error_i.append(error)
+
                 self.w[i] = OW
                 self.preiS[i]=iS
 
                 tempH = delta_X.dot(OW.T)
-                delta_X = ActivationFunc(tempH,self.actFun[i],self.para[i])
+                delta_X = self.activeFunction(tempH, self.actFun)
                 delta_X = preprocessing.scale(delta_X.T)
                 delta_X = delta_X.T
 
-
-    def feature_extrackted(self,x):
-        '''
-        自编码器的特征提取
-        '''
+    def feature_extrackted(self, x):
         feature = x
         for i in range(self.layers):
             feature = feature.dot(self.w[i].T)
-            feature = ActivationFunc(feature, self.actFun[i], self.para[i])
+            feature = self.activeFunction(feature, self.actFun)
         return feature
 
+    def predict(self, X_train, X_test, y_train, y_test):
+        train_feature = pilae.feature_extrackted(X_train)
+        test_feature = pilae.feature_extrackted(X_test)
+        model = self.regression_classifier(train_feature, y_train)
+        train_predict = model.predict(train_feature)
+        self.train_acc = accuracy_score(train_predict, y_train) * 100
+        print("Accuracy of train data set: %.2f" % self.train_acc, "%")
+        test_predict = model.predict(test_feature)
+        self.test_acc = accuracy_score(test_predict, y_test) * 100
+        print("Accuracy of test data set: %.2f" % self.test_acc, "%")
 
-def  ActivationFunc(tempH, ActivationFunction, p):
-    def relu(x,p):
-        x[x<0]=0
-        return x
-    switch ={
-        'sig' : lambda x,p:1/(1+np.exp(-p*x)),
-        'sin' : lambda x,p:np.sin(x),
-        'relu' : relu,
-        'srelu' : lambda x,p : np.log(1+np.exp(x)),
-        'tanh' : lambda x,p : np.tanh(p*x),
-        # 'max' : lambda x,p : np.max(0, x),
-    }
-    fun = switch.get(ActivationFunction)
-    return fun(tempH,p)
+    def regression_classifier(self, train_X, train_y):
+        from sklearn.linear_model import LogisticRegression
+        model = LogisticRegression(solver="lbfgs", multi_class="multinomial", max_iter=200)
+        model.fit(train_X, train_y)
+        return model
 
+    def svm_classifier(self, train_X, train_y):
+        from sklearn.svm import SVC
+        model = SVC(kernel='linear', probability=False)
+        model.fit(train_X, train_y)
+        return model
+
+    def linearsvm_classifier(self, train_X, train_y):
+        from sklearn import svm
+        model = svm.LinearSVC()
+        model.fit(train_X, train_y)
+        return model
+
+    def activeFunction(self, tempH, func='sig', p=1.):
+        def relu(x, p):
+            x[x < 0] = 0
+            return x
+
+        switch = {
+            'sig': lambda x, p: 1 / (1 + np.exp(-p * x)),
+            'sin': lambda x, p: np.sin(x),
+            'relu': relu,
+            'srelu': lambda x, p: np.log(1 + np.exp(x)),
+            'tanh': lambda x, p: np.tanh(p * x),
+        }
+        fun = switch.get(func)
+        return fun(tempH, p)
 
 
 if __name__ == '__main__':
 
-
     batchsize = 2000
     HiddernNeurons = [600]
-    k = [1e-7, 1e-4, 1e-5]
-    para = [1.5, 1.5, 1]
-    actFun = ['tanh', 'tanh', 'tanh', 'tanh', 'tanh']
-    for i in range(0, 10):
-        # HiddernNeurons.append(700 - i * 50)
-        k.append(1e-3)
-        para.append(1.5)
-        actFun.append('tanh')
-
-
+    k = [1.2]
 
 # mnist data++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    (X_train, y_train), (X_test, y_test) = load_data("../data/mnist.npz")
-
-
-
-
+    (X_train, y_train), (X_test, y_test) = tools.load_npz("../dataset/mnist/mnist.npz")
+    X_train = X_train.reshape(-1, 784).astype('float32')/255
+    X_test = X_test.reshape(-1, 784).astype('float32')/255
 
     t1 = time.time()
-    pilae = PILAE(HiddernNeurons, batchsize, k, para, actFun)  # 初始化PILAE模型
-    pilae.train(X_train)  # 训练模型
+    pilae = PILAE(HiddernNeurons, batchsize, k, actFun='sig')
+    pilae.fit(X_train)
+    pilae.predict(X_train, X_test, y_train, y_test)
     t2 = time.time()
     print("time cost of training PILAE: %.4f" % (t2 - t1))
 
-    train_feature = pilae.feature_extrackted(X_train)  # 提取特征训练集
-    test_feature = pilae.feature_extrackted(X_test)  # 提取特征测试集
-
-    # 特征分类
-    reg = LogisticRegression(solver="lbfgs", multi_class="multinomial")
-
-    reg.fit(train_feature, y_train)
-    # np.savetxt('coef_softmax_sklearn.txt', reg.coef_, fmt='%.6f')  # Save coefficients to a text file
-    ptrain = reg.predict(train_feature)
-    print("Accuracy of train set: %f" % accuracy_score(ptrain, y_train))
-
-    # reg.fit(testfeatureofdate, Label2)
-    ptest = reg.predict(test_feature)
-    print("Accuracy of test set: %f" % accuracy_score(ptest, y_test))
 
 
 
