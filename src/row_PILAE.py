6cb0f30c09a68e85094922e16cf6acb7ce962d7f
sibofeng <fengsiboacer@gmail.com>
2017-10-29 16:26:31 +0800

    add w_e losserror and then

--- src/row_PILAE.py
+++ src/row_PILAE.py
@@ -2,10 +2,11 @@ import numpy as np
 import math
 import time
 from sklearn import preprocessing
+from sklearn.metrics import mean_squared_error
 import src.tools as tools
 
 
-class row_PILAE(object):
+class PILAE(object):
     def __init__(self, k=0.5, alpha = 0.8, beta=0.9, activeFunc='tanh'):
         self.k = k
         self.alpha = alpha
@@ -16,47 +17,44 @@ class row_PILAE(object):
         self.acFunc = activeFunc
         self.weight = []
 
-    def activeFunction(self, tempH, func='sig', p=1.):
-        def relu(x, p):
-            x[x < 0] = 0
-            return x
-
+    def activeFunction(self, tempH, func='sig'):
         switch = {
-            'sig': lambda x, p: 1 / (1 + np.exp(-p * x)),
-            'sin': lambda x, p: np.sin(x),
-            'relu': relu,
-            'srelu': lambda x, p: np.log(1 + np.exp(x)),
-            'tanh': lambda x, p: np.tanh(p * x),
-            # 'max' : lambda x,p : np.max(0, x),
+            'sig': lambda x: 1 / (1 + np.exp(-x)),
+            'sin': lambda x: np.sin(x),
+            'srelu': lambda x: np.log(1 + np.exp(x)),
+            'tanh': lambda x: np.tanh(x),
+            'swish':lambda x: x/(1 + np.exp(-x)),
+            'relu' : lambda x: np.max((0, x)),
         }
         fun = switch.get(func)
-        return fun(tempH, p)
+        return fun(tempH)
 
     def autoEncoder(self, input_X, layer):
         self.layer = layer
         t1 = time.time()
-        U, s, V = np.linalg.svd(input_X, full_matrices=0)
-        print("the ", layer, " layer SVD matricx shape:", "U:", U.shape, "s:", s.shape, "V:", V.shape)  #(784, 784) (784,) (784, 60000)
+        U, s, transV = np.linalg.svd(input_X, full_matrices=0)
+        print("the ", layer, " layer SVD matrix shape:", "U:", U.shape, "s:", s.shape, "V:", transV.shape)  #(784, 784) (784,) (784, 60000)
         dim_x = input_X.shape[1]
         rank_x = np.sum(s > 1e-3)
         print("the ", layer, " layer, dim_x:", dim_x, " rank_x:", rank_x)
         S = np.zeros((U.shape[1], V.shape[0]))
         S[:s.shape[0], :s.shape[0]] = np.diag(s)
-        V = V.T
-        U = U.T
+        V = transV.T
+        transU = U.T
         S[S != 0] = 1 / S[S != 0]
         if rank_x < dim_x :
-            p = rank_x + self.alpha*(dim_x-rank_x)
+            p = rank_x + self.alpha*(dim_x - rank_x)
             # p = self.beta*dim_x
         else:
             p = self.beta*dim_x
         # p = self.beta*dim_x
         print("the ", layer, " layer, cut p:", int(p))
-        U = U[:, 0:int(p)]
-        print("the ", layer, " layer pseduinverse matricx shape:", "U:", U.shape, "S:", S.shape, "V:", V.shape) #(705, 784) (784, 784) (784, 784)
-        W_e = V.dot(S).dot(U)
+        transU = transU[:, 0:int(p)]
+        print("the ", layer, " layer psedoinverse matrix shape:", "U:", transU.shape, "S:", S.shape, "V:", V.shape) #(705, 784) (784, 784) (784, 784)
+        # W_e = V.dot(S).dot(transU)
+        input_H = U.dot(transU)
 
-        H = self.activeFunction(input_X.dot(W_e), self.acFunc)
+        H = self.activeFunction(input_H, self.acFunc)
         invH = np.linalg.inv(H.T.dot(H) + np.eye(H.shape[1]) * self.k)
         W_d = invH.dot(H.T).dot(input_X)
         t2 = time.time()
@@ -70,12 +68,16 @@ class row_PILAE(object):
             w = self.autoEncoder(X, i)
             self.weight.append(w)
             H = self.activeFunction(X.dot(w), self.acFunc)
-            H = H/(H.max() - H.min())
-            O = H.dot(w.T)
-            square = np.linalg.norm(X - O)
-            meanSquareError = square**2/X.size
+            invH = np.linalg.inv(H.T.dot(H) + np.eye(H.shape[1]) * self.k) # recompute W_d
+            W_d = invH.dot(H.T).dot(X)
+            # H = H/(H.max() - H.min())
+            O = H.dot(W_d)
+            meanSquareError = mean_squared_error(X, O)
+            print("the ", i, " layer meanSquareError:%.2f" % meanSquareError)
+            lossF = H.dot(np.linalg.pinv(H)) - np.ones((H.shape[0], H.shape[0]))
+            error = np.linalg.norm(lossF)
+            print("the ", i, " layer lossError:%.2f" % error)
             X = H
-            print("the ", i, " layer meanSquareError:%.2f" %meanSquareError)
 
         t2 = time.time()
         print("fit cost time :%.2f" %(t2 - t1))
@@ -94,8 +96,7 @@ class row_PILAE(object):
         from sklearn.metrics import accuracy_score
         train_feature = self.extractFeature(train_X)
         test_feature = self.extractFeature(test_X)
-        # tools.save_pickle(train_feature, "../data/one_train_feature.plk")
-        # tools.save_pickle(test_feature, "../data/one_test_feature.plk")
+
         model = self.regression_classifier(train_feature, train_y)
         train_predict = model.predict(train_feature)
         self.train_acc = accuracy_score(train_predict, train_y)*100
--- src/test.py
+++ src/test.py
@@ -1,5 +1,7 @@
 import sys
-sys.path.append("../src")
+import os
+workpath = os.path.abspath("..")
+sys.path.append(workpath)
 import src.row_PILAE as rp
 import src.tools as tools
 import src.Hog as hg
@@ -11,14 +13,22 @@ import csv
 
 
 (X_train, y_train), (X_test, y_test) = tools.load_npz("../dataset/mnist/mnist.npz")
-X_train = X_train.reshape(-1, 784).astype('float32')/255 - 0.5
-X_test = X_test.reshape(-1, 784).astype('float32')/255 - 0.5
+train_data = np.concatenate((X_train, X_test), axis=1)
+n, p, q = train_data.shape
+train_data -= np.mean(train_data, axis=1)
+train_data /= np.std(train_data, axis=1)
+X_train = train_data[0: 60000]
+X_test = train_data[60000: 70000]
+# X_train = X_train.reshape(-1, p*q).astype('float32')/255
+# X_test = X_test.reshape(-1, 784).astype('float32')/255
 
-# for hog data
-num = 15
-X_train, X_test = hg.load_hog("../data/mnist/mnist", num)
-X_train *= 100
-X_test *= 100
+# # for hog data
+# # for i in range(1, 27):
+num = 0
+# print("the "+str(num)+" scriptor")
+# X_train, X_test = hg.load_hog("../data/mnist/mnist", 1, num)
+# X_train *= 10
+# X_test *= 10
 
 # print(X_train.shape, X_test.shape)
 
@@ -42,14 +52,14 @@ X_test *= 100
 # X_test = preprocessing.scale(X_test, axis=1)
 
 t1 = time.time()
-pilae = rp.row_PILAE(k=2, alpha=0.8, beta=0.8, activeFunc='tanh')
+pilae = rp.PILAE(k=2.5, alpha=0.9, beta=0.8, activeFunc='tanh')
 pilae.fit(X_train, layer=1)
 pilae.predict(X_train, y_train, X_test, y_test)
 t2 = time.time()
 cost_time = t2 - t1
 print("Total cost time: %.2f" %cost_time)
 # write into .csv file
-with open("../log/fashion_mnist_info.csv", 'at+') as csvfile:
+with open("../log/mnist.csv", 'at+') as csvfile:
     writer = csv.writer(csvfile)
-    writer.writerow(["map", "dimesion", "layer", "time", "train_acc", "test_acc", "k", "alpha", "beta", "acf"])
-    writer.writerow([num, X_train[1], pilae.layer, cost_time, pilae.train_acc, pilae.test_acc, pilae.k, pilae.alpha, pilae.beta, pilae.acFunc])
+    writer.writerow(["map", "dims", "layer", "time", "train_acc", "test_acc", "k", "alpha", "beta", "acf"])
+    writer.writerow([num, X_train.shape[1], pilae.layer, cost_time, pilae.train_acc, pilae.test_acc, pilae.k, pilae.alpha, pilae.beta, pilae.acFunc])
